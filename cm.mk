eb1cd9270d8baa40deee8a7725b8b29bdf93cac1
David Rouleau <Shabbypenguin@gmail.com>
2012-08-23 08:59:58 -0400

    initial

---
+++ cm.mk
@@ -0,0 +1,18 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/cdma.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := xt897
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/moto/xt897/device_xt897.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := xt897
+PRODUCT_NAME := cm_xt897
+PRODUCT_BRAND := moto
+PRODUCT_MODEL := xt897
+PRODUCT_MANUFACTURER := moto
