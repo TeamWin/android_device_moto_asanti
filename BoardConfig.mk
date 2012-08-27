c4b0b45854abb7e2c3db8d0b69bb37d6df041f37
David Rouleau <Shabbypenguin@gmail.com>
2012-08-27 17:36:03 -0400

    stop misc errors

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,3 +27,6 @@ TARGET_PREBUILT_KERNEL := device/moto/xt897/kernel
 #BOARD_HAS_NO_SELECT_BUTTON := true
 # Use this flag if the board has a ext4 partition larger than 2gb
 #BOARD_HAS_LARGE_FILESYSTEM := true
+
+#TWRP
+DEVICE_RESOLUTION := 540x960
