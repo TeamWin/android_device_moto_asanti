1444905d63e45b8603cf68d29617204bc0cf2b0c
David Rouleau <Shabbypenguin@gmail.com>
2012-08-27 16:40:17 -0400

    Fix ramdisk issue

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,8 +8,9 @@ TARGET_BOARD_PLATFORM := unknown
 TARGET_CPU_ABI := armeabi
 TARGET_BOOTLOADER_BOARD_NAME := xt897
 
-BOARD_KERNEL_CMDLINE := console=/dev/null androidboot.hardware=qcom user_debug=31 loglevel=1 msm_rtb.filter=0x3F kgsl.mmutype=gpummu
-BOARD_KERNEL_BASE := 0x80200000
+BOARD_KERNEL_CMDLINE        := console=/dev/null androidboot.hardware=qcom user_debug=31 loglevel=1 msm_rtb.filter=0x3F kgsl.mmutype=gpummu
+BOARD_KERNEL_BASE           := 0x80200000
+BOARD_FORCE_RAMDISK_ADDRESS := 0x81600000
 BOARD_KERNEL_PAGESIZE := 2048
 
 # fix this up by examining /proc/mtd on a running device
@@ -18,6 +19,8 @@ BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x105c0000
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x105c0000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
 BOARD_FLASH_BLOCK_SIZE := 131072
+
+TARGET_USERIMAGES_USE_EXT4 := true
 
 TARGET_PREBUILT_KERNEL := device/moto/xt897/kernel
 
